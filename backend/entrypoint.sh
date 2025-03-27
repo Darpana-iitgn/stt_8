c99965a28e85776ab4e98b174223fa46d0b99633
Darpana-IITGN <23110085@iitgn.ac.in>
2025-03-27 22:01:20 +0530

    clrf

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
-  #!/bin/sh
-  set -e
+#!/bin/sh
+set -e
 
-  echo "Starting Backend Service..."
-  exec uvicorn main:app --host 0.0.0.0 --port 9567
+echo "Starting Backend Service..."
+exec uvicorn main:app --host 0.0.0.0 --port 9567
